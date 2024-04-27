1421a2f17140cc52fc2722049a5824e916834c67
Siepert123 <sj.hijmans@gmail.com>
2024-04-27 09:26:34 +0200

    i mnake typo oupsie

--- common/src/main/java/us/randos/very_serious_steven/VerySeriousStevenCommon.java
+++ common/src/main/java/us/randos/very_serious_steven/VerySeriousStevenCommon.java
@@ -1,14 +1,5 @@
 package us.randos.very_serious_steven;
-
-// This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
-// import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
-// common compatible binaries. This means common code can not directly use loader specific concepts such as Forge events
-// however it will be compatible with all supported mod loaders.
 public class VerySeriousStevenCommon {
-
-    // The loader specific projects are able to import and use any code from the common project. This allows you to
-    // write the majority of your code here and load it from your loader specific projects. This example has some
-    // code that gets invoked by the entry point of the loader specific projects.
     public static void init() {
         VerySeriousStevenConstants.LOGGER.info(VerySeriousStevenConstants.VERY_SERIOUS_MOD_NAME_FOR_THE_COOLEST_MOD_IN_EXISTENCE_NAMED_VERY_SERIOUS_STEVEN_MOD_FOR_THE_KAUPENHUB_SERVER + " has registered its' main class in the common env.");
     }
--- forge/src/main/java/us/randos/very_serious_steven/VerySeriousStevenForge.java
+++ forge/src/main/java/us/randos/very_serious_steven/VerySeriousStevenForge.java
@@ -1,23 +1,47 @@
 package us.randos.very_serious_steven;
 
 import eu.midnightdust.lib.config.MidnightConfig;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import us.randos.very_serious_steven.config.VerySeriousConfigForge;
+import us.randos.very_serious_steven.siepertArea.registration.SiepertBlockItems;
+import us.randos.very_serious_steven.siepertArea.registration.SiepertBlocks;
+import us.randos.very_serious_steven.siepertArea.registration.SiepertItems;
 
 // TODO: Any of the Forge contributors, please port the Mask Of Seriousness to Forge side, use Curios API as a replacement for Trinkets, Pekhui also works the same both sides.
 @Mod(VerySeriousStevenConstants.VERY_SERIOUS_MOD_ID_FOR_THE_COOLEST_MOD_IN_EXISTENCE_NAMED_VERY_SERIOUS_STEVEN_MOD_FOR_THE_KAUPENHUB_SERVER)
 public class VerySeriousStevenForge {
     
     public VerySeriousStevenForge() {
-    
-        // This method is invoked by the Forge mod loader when it is ready
-        // to load your mod. You can access Forge and Common code in this
-        // project.
-    
-        // Use Forge to bootstrap the Common mod.
         VerySeriousStevenConstants.LOGGER.info(VerySeriousStevenConstants.VERY_SERIOUS_MOD_NAME_FOR_THE_COOLEST_MOD_IN_EXISTENCE_NAMED_VERY_SERIOUS_STEVEN_MOD_FOR_THE_KAUPENHUB_SERVER + " has registered its' main class in the forge env.");
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        SiepertBlocks.register(modEventBus);
+        SiepertBlockItems.register(modEventBus);
+        SiepertItems.register(modEventBus);
+
         VerySeriousStevenCommon.init();
-        // Config
+
         MidnightConfig.init(VerySeriousStevenConstants.VERY_SERIOUS_MOD_ID_FOR_THE_COOLEST_MOD_IN_EXISTENCE_NAMED_VERY_SERIOUS_STEVEN_MOD_FOR_THE_KAUPENHUB_SERVER, VerySeriousConfigForge.class);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(SiepertBlockItems.DESTABILIZED_SHAPE.get());
+        }
     }
 }
